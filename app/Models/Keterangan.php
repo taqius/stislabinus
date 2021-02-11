4d853e8b1920518534cbaa6fec6a510ab7ea1f83
taqius <taqiussa@gmail.com>
2021-02-11 22:37:02 +0700

    model untuk binus

---
+++ app/Models/Keterangan.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Keterangan extends Model
+{
+    use HasFactory;
+    protected $table = 'keterangan';
+    protected $guarded = [];
+}
