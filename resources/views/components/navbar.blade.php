4d4e55a363b16caea04a0003c35736bc2182c38a
taqius <taqiussa@gmail.com>
2021-02-21 11:07:07 +0700

    profile photo

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -4,16 +4,19 @@ $user = auth()->user();
 
 <div class="navbar-bg"></div>
 <nav class="navbar navbar-expand-lg main-navbar">
-    <form class="form-inline mr-auto">
-        <ul class="navbar-nav mr-3">
+    <form class="mr-auto form-inline">
+        <ul class="mr-3 navbar-nav">
             <li><a href="#" data-turbolinks="false" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
         </ul>
-        <h1 class="font-weight-bold text-2xl text-white">{{ config('app.name', 'Laravel') }}</h1>
+        <h1 class="text-2xl text-white font-weight-bold">{{ config('app.name', 'Laravel') }}</h1>
     </form>
     <ul class="navbar-nav navbar-right">
         <li class="dropdown"><a href="#" data-turbolinks="false" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
             @if (!is_null($user))
-                <div class="d-sm-none d-lg-inline-block">Hi, {{ $user->name }}</div></a>
+            <div class="d-sm-none d-lg-inline-block">Hi, {{ $user->name }}
+                {{-- <img src="{{ asset('storage/'.$user->profile_photo_path) }}" alt="{{ $user->name }}" class="rounded-lg"> --}}
+            </div>
+                </a>
             @else
                 <div class="d-sm-none d-lg-inline-block">Hi, Welcome</div></a>
             @endif
