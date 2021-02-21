96df264bfff68b57c2470503ad7999051d707200
taqius <taqiussa@gmail.com>
2021-02-21 11:22:10 +0700

    make menu list dan update sidebar

---
+++ database/migrations/2021_02_21_110951_create_menus_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->text('section');
+            $table->text('list_menu');
+            $table->text('list_sub_menu');
+            $table->foreignId('role_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
