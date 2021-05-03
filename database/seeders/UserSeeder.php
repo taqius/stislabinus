faa83851ddc7415b735fa78183bcbf124f5440de
taqius <taqiussa@gmail.com>
2021-05-03 10:38:30 +0700

    user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,119 +14,18 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $usernya = [
-            [
-                'name' => 'Eka Aribawa,S.Pd.I',
-                'email' => 'arieka@binusasmg.sch.id',
-                'password' => bcrypt('12345678'),
-                'created_at' => now(), 'updated_at' => now(),
-            ],
-            [
-                'name' => 'Lutfi Pujianti,S.E',
-                'email' => 'lutfi@binusasmg.sch.id',
-                'password' => bcrypt('12345678'),
-                'created_at' => now(), 'updated_at' => now(),
+        $admin = User::create([
+            'name' => 'Admin Role',
+            'email' => 'admin@role.test',
+            'password' => bcrypt('12345678')
+        ]);
+        $admin->assignRole('admin');
 
-            ],
-            [
-                'name' => 'Tony Kusetiyono,S.E.',
-                'email' => 'tonyk@binusasmg.sch.id',
-                'password' => bcrypt('12345678'),
-                'created_at' => now(), 'updated_at' => now(),
-
-            ],
-            [
-                'name' => 'Isti Malinda,S.Pd',
-                'email' => 'linda@binusasmg.sch.id',
-                'password' => bcrypt('12345678'),
-                'created_at' => now(), 'updated_at' => now(),
-
-            ],
-            [
-                'name' => 'Aroem Santi Litania,S.Pd',
-                'email' => 'aroemlit@binusasmg.sch.id',
-                'password' => bcrypt('12345678'),
-                'created_at' => now(), 'updated_at' => now(),
-
-            ],
-            [
-                'name' => 'Dianing Ratri O,S.Pd',
-                'email' => 'dianingratrio@binusasmg.sch.id',
-                'password' => bcrypt('12345678'),
-                'created_at' => now(), 'updated_at' => now(),
-
-            ],
-            [
-                'name' => 'Tito Dwi Yulianto,S.Pd,Gr.',
-                'email' => 'tito@binusasmg.sch.id',
-                'password' => bcrypt('12345678'),
-                'created_at' => now(), 'updated_at' => now(),
-
-            ],
-            [
-                'name' => 'Ida Fahruroziyah,S.Pd',
-                'email' => 'idafahruroz@binusasmg.sch.id',
-                'password' => bcrypt('12345678'),
-                'created_at' => now(), 'updated_at' => now(),
-
-            ],
-            [
-                'name' => 'Khusnul Khotimah,S.Pd',
-                'email' => 'khusnul@binusasmg.sch.id',
-                'password' => bcrypt('12345678'),
-                'created_at' => now(), 'updated_at' => now(),
-
-            ],
-            [
-                'name' => 'Askuriati,S.E',
-                'email' => 'askuriati@binusasmg.sch.id',
-                'password' => bcrypt('12345678'),
-                'created_at' => now(), 'updated_at' => now(),
-
-            ],
-            [
-                'name' => 'Soimatun,S.Pd',
-                'email' => 'soimatun@binusasmg.sch.id',
-                'password' => bcrypt('12345678'),
-                'created_at' => now(), 'updated_at' => now(),
-
-            ],
-            [
-                'name' => 'Siti Masruroh,A.Md',
-                'email' => 'rurohjio@binusasmg.sch.id',
-                'password' => bcrypt('12345678'),
-                'created_at' => now(), 'updated_at' => now(),
-
-            ],
-            [
-                'name' => 'Rufri Tri Irianti,S.Pd',
-                'email' => 'ruficivic@binusasmg.sch.id',
-                'password' => bcrypt('12345678'),
-                'created_at' => now(), 'updated_at' => now(),
-
-            ],
-            [
-                'name' => 'Indah Dwi Putri Banjarsari,S.Pd',
-                'email' => 'indahdwi@binusasmg.sch.id',
-                'password' => bcrypt('12345678'),
-                'created_at' => now(), 'updated_at' => now(),
-
-            ],
-            [
-                'name' => 'Indah Noviani,S.Pd',
-                'email' => 'indahnope@binusasmg.sch.id',
-                'password' => bcrypt('12345678'),
-                'created_at' => now(), 'updated_at' => now(),
-
-            ],
-            [
-                'name' => 'Arif Adi Wijaya,S.Pd',
-                'email' => 'arifadiwijaya@binusasmg.sch.id',
-                'password' => bcrypt('12345678'),
-                'created_at' => now(), 'updated_at' => now(),
-
-            ],
-        ];
-        User::insert($usernya);
+        $user = User::create([
+            'name' => 'user Role',
+            'email' => 'user@role.test',
+            'password' => bcrypt('12345678')
+        ]);
+        $user->assignRole('user');
     }
 }
